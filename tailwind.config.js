a8750ee7f2716989c3fd0b12ba5e476787ae2b07
Rebekah Weathers <hlessirah@gmail.com>
2023-11-30 15:44:19 -0700

    added mobile format changes on all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./views/*.ejs",],
   theme: {
     extend: {},
+    screens: {
+      'lg': {'min': '768px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [require("daisyui")],
   daisyui: {
